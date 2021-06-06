9f7416ff7ac5898498ad261f0631b441afe0e313
Katie Hyewon Choi <hyewonchoi31@gmail.com>
2021-06-06 19:05:03 -0400

    commit tree 85f804c506433f16d9bc3f7da2a32758aa48a84e

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,12 @@
 class CheesesController < ApplicationController
-
   def index
     cheeses = Cheese.all
     render json: cheeses
   end
 
+  def show
+    # byebug ##then run params in terminal
+    cheese = Cheese.find(params[:id])
+    render json: cheese
+  end
 end
